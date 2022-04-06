ad99ba6126641c61350148a6ca2893d1100ed24e
ms-O <kristin.obermyer@villanova.edu>
2022-04-06 14:26:20 -0400

    Add sample test and implementation for convert()

---
+++ src/CurrencyConverterTest.java
@@ -0,0 +1,29 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+public class CurrencyConverterTest {
+
+    @Test
+    public void testConvert() {
+        CurrencyConverter converter = new CurrencyConverter();
+        BigDecimal originalAmount = BigDecimal.valueOf(100.00);
+        BigDecimal rate = BigDecimal.valueOf(2.70);
+        BigDecimal expectedAmount = BigDecimal.valueOf(270.00).setScale(2);
+        BigDecimal actualAmount = converter.convert(originalAmount, rate);
+        assertEquals(expectedAmount, actualAmount);
+    }
+    
+    @Test
+    public void testConvertWithRounding() {
+        CurrencyConverter converter = new CurrencyConverter();
+        BigDecimal originalAmount = BigDecimal.valueOf(99.99);
+        BigDecimal rate = BigDecimal.valueOf(0.055);
+        BigDecimal expectedAmount = BigDecimal.valueOf(5.50).setScale(2);
+        BigDecimal actualAmount = converter.convert(originalAmount, rate);
+        assertEquals(expectedAmount, actualAmount);
+    }
+
+}
